0b08b4d404fdc3fd818c1897b034144c9a8cc590
Вайцеховский Антон <alfach23@gmail.com>
2017-06-19 12:28:51 +0700

    Авторизация и Основа

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=teachers',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
